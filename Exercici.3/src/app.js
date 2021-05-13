cbaa7b8e88f7207c991943917e3fe02ecdbc3ba5
Anna Montserrat <amp1281@gmail.com>
2021-05-13 19:05:41 +0200

    ok

--- Exercici.3/src/app.js
+++ Exercici.3/src/app.js
@@ -1,5 +1,3 @@
-import {message} from "./modulos/message";
-import {alertas} from "./modulos/alertas";
-import {guardar} from "./modulos/guardar";
-import {product} from "./modulos/product";
+import {mensajeAlerta} from "./modulos/test";
 
+mensajeAlerta ("Hola Mundo");
